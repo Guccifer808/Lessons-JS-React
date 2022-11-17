959abee55230b3e0c1af17c362b3a114fbdcc201
Guccifer808 <36012294+Guccifer808@users.noreply.github.com>
2022-11-17 15:29:38 +0200

    JSON(stringify, parse), clone

--- L51 - JSON/script.js
+++ L51 - JSON/script.js
@@ -10,5 +10,20 @@ const person = {
 console.log(JSON.stringify(person));
 
 //parse JSON (from server)
-
 console.log(JSON.parse(JSON.stringify(person)));
+
+// cloning object
+const persone = {
+  name: "John",
+  tel: "+123",
+  parents: {
+    mom: "Nana",
+    dad: "Mike",
+  },
+};
+
+const clone = JSON.parse(JSON.stringify(persone));
+
+clone.parents.mom = "Anna";
+console.log(persone);
+console.log(clone);
